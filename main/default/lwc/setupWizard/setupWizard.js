a9a8a0bd5fc5bec7f26159c5833abecfbecd43e0
ddzemidau1path <ddzemidau@1path.com>
2019-04-15 13:13:28 +0300

    User Login

--- main/default/lwc/setupWizard/setupWizard.js
+++ main/default/lwc/setupWizard/setupWizard.js
@@ -1,3 +1,37 @@
-import { LightningElement } from 'lwc';
+import {LightningElement, track, wire, api} from 'lwc';
+import invokeAction from '@salesforce/apex/ActionControllerTemplate.invokeAction';
 
-export default class SetupWizard extends LightningElement {}
+export default class SetupWizard extends LightningElement {
+    @track username;
+    @track password;
+    @track userRole
+
+    handleUsernameChange(event) {
+        this.username = event.target.value;
+    }
+
+    handlePasswordChange(event) {
+        this.password = event.target.value;
+    }
+
+    handleLogin() {
+        var action = [{
+            className : 'LoginController',
+            action : 'user-login',
+            args : { username: this.username, password: this.password }
+        }];
+
+        var strAction = JSON.stringify(action);
+
+        invokeAction({
+            actions : strAction
+        }).then(result => {
+            console.log('Your role: ' + result);
+            this.userRole = result;
+        }).catch(error => {
+           console.log('err' + JSON.stringify(error));
+        });
+    }
+
+
+}
