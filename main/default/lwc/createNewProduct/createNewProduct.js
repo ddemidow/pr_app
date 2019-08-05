8f62001674301ec29a0778e9f6fffc8b21761f9e
ddzemidau <dzmitry.dzemidau@itechart-group.com>
2019-08-05 12:34:08 +0300

    Utils and Product trigger

---
+++ main/default/lwc/createNewProduct/createNewProduct.js
@@ -0,0 +1,4 @@
+import { LightningElement, api } from 'lwc';
+export default class CreateNewProduct extends LightningElement {
+
+}
--- main/default/lwc/productListSection/productListSection.js
+++ main/default/lwc/productListSection/productListSection.js
@@ -54,7 +54,7 @@ export default class ProductListSection extends LightningElement {
     }
 
     connectedCallback() {
-        this.showSpinner();
+        //this.showSpinner();
     }
 
     filtersMode() {
